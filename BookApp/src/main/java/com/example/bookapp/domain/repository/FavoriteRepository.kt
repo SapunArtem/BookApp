0157fb1e26685f8326480dab0fbd5fe73aa53e5e
SapunArtem <sapun_artem@mail.ru>
2025-08-18 16:10:57 +0300

    changes in domain FavoriteRepository

--- BookApp/src/main/java/com/example/bookapp/domain/repository/FavoriteRepository.kt
+++ BookApp/src/main/java/com/example/bookapp/domain/repository/FavoriteRepository.kt
@@ -5,6 +5,7 @@ import com.example.bookapp.domain.models.Book
 interface FavoriteRepository {
     suspend fun getAllFavorites(): List<Book>
     suspend fun addFavorite(book: Book)
-    suspend fun removeFavorite(bookId : String)
-    suspend fun isFavorite(bookId: String) : Boolean
+    suspend fun removeFavorite(bookId: String)
+    suspend fun isFavorite(bookId: String): Boolean
+    suspend fun getFavoriteDetails(bookId: String): Book?
 }
