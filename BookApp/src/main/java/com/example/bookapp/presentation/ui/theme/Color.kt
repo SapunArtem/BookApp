a01ff4ccc8413511188a9073140599fbb7d701bd
SapunArtem <sapun_artem@mail.ru>
2025-08-18 15:25:54 +0300

    add stringResources add colors

--- BookApp/src/main/java/com/example/bookapp/presentation/ui/theme/Color.kt
+++ BookApp/src/main/java/com/example/bookapp/presentation/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.bookapp.ui.theme
+package com.example.bookapp.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Orange = Color(0xFFFB9722)
+val GrayUnselected = Color(0x80FFFFFF)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF1F1F1F)
